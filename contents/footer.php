0d79c970990b77728e39879df4c13a00717603b1
Vazenroy <vazenroy830@gmail.com>
2026-04-10 11:26:14 -0600

    removed php

--- contents/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-    echo '<footer>
-            <a href="mailto:csudraghouseofovis@gmail.com" title="Email Us">Email</a>
-            <p style="display: inline;">|</p>
-            <a href="https://www.instagram.com/csudraghouseofovis/" target="_blank" title="Check out our Instagram">Instagram</a>
-            <p style="display: inline;">|</p>
-            <a href="https://teamup.com/ks9px76vgnh9s7scsv" target="_blank">Calendar</a>
-        </footer>';
-?>
--- contents/head_details.php
+++
@@ -1,6 +0,0 @@
-<?php
-    echo '<link rel="stylesheet" href="ovis_style.css">
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta name="author" content="Van Montgomery">
-        <meta name="keywords" content="drag, ovis, colorado state university">';
-?>
