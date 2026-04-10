1cc6228e065dc0896ca0c0cb6171767600bb0795
Vazenroy <vazenroy830@gmail.com>
2026-04-09 21:41:42 -0600

    moved stuff to contents

---
+++ contents/head_details.php
@@ -0,0 +1,6 @@
+<?php
+    echo '<link rel="stylesheet" href="ovis_style.css">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="author" content="Van Montgomery">
+        <meta name="keywords" content="drag, ovis, colorado state university">';
+?>
---
+++ contents/navbar.php
@@ -0,0 +1,10 @@
+<?php
+    echo'
+        <nav>
+            <a href="index.html">Homepage</a>
+            <p style="display: inline;">|</p>
+            <a>About Us</a>
+            <p style="display: inline;">|</p>
+            <a>Upcoming Events</a>
+        </nav>';
+?>
